d8cb054ee24ddd4ebd0e6ba39bcf92caf499843c
Kewl <87680132+ac8736@users.noreply.github.com>
2022-11-16 16:37:41 -0500

    doing some refactoring

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify, request, session
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pymysql.cursors
 import bcrypt
+import jwt
 
 app = Flask(__name__)
+
 CORS(app)
 conn = pymysql.connect(host='localhost',
                        user='root',
@@ -11,6 +13,7 @@ conn = pymysql.connect(host='localhost',
                        db='airline_ticket_reservation',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
+
 
 @app.route("/all-flights", methods=['GET'])
 def all_flights():
@@ -25,94 +28,66 @@ def all_flights():
 @app.route("/register-customer", methods=["GET", 'POST'])
 def register_customer():
     cursor = conn.cursor()
-    full_name = request.json["fullName"]
-    email = request.json['email']
-    password = request.json['password']
-    dob = request.json['dob']
-    phone_number = request.json["phoneNumber"]
-    building_number = request.json['buildingNumber']
-    street = request.json['street']
-    city = request.json['city']
-    state = request.json['state']
-    passport_number = request.json['passportNumber']
-    passport_expiration = request.json['passportExpiration']
-    passport_country = request.json['passportCountry']
+    new_account = request.json
     query = "SELECT * FROM customer WHERE email=%s"
-    cursor.execute(query, (email))
-    user = cursor.fetchone()
-    if user:
+    cursor.execute(query, (new_account["email"]))
+    if cursor.fetchone():
         return jsonify({'status': 'Email already in use.'}), 409
     query = "INSERT INTO customer VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-    cursor.execute(query, (email, full_name, password, building_number, street, city, state, phone_number, passport_number, passport_expiration, passport_country, dob))
+    hashed_password = bcrypt.hashpw(new_account["password"].encode('utf8'), bcrypt.gensalt())
+    cursor.execute(query, (new_account["email"], new_account["full_name"], hashed_password, new_account["building_number"], 
+                           new_account["street"], new_account["city"], new_account["state"], new_account["phone_number"], 
+                           new_account["passport_number"], new_account["passport_expiration"], new_account["passport_country"], 
+                           new_account["dob"]))
     conn.commit()
     cursor.close()
-    session['username'] = email
     return jsonify({'status': 'Successfully registered customer.'}), 200
 
 
 @app.route("/register-airline-staff", methods=["GET", "POST"])
 def register_airline_staff():
     cursor = conn.cursor()
-    username = request.json["username"]
-    password = request.json["password"]
-    first_name = request.json["firstName"]
-    last_name = request.json["lastName"]
-    dob = request.json["dob"]
-    airline = request.json["airline"]
+    new_account = request.json
     query = "SELECT * FROM airline_staff WHERE username=%s"
-    cursor.execute(query, (username))
-    user = cursor.fetchone()
-    if user:
+    cursor.execute(query, (new_account["username"]))
+    if cursor.fetchone():
         return jsonify({'status': 'User has already been registered.'}), 409
     query = "SELECT * FROM airline WHERE name=%s"
-    cursor.execute(query, (airline))
-    airline_exist = cursor.fetchone()
-    if not airline_exist:
+    cursor.execute(query, (new_account["airline"]))
+    if not cursor.fetchone():
         return jsonify({'status': 'Airline does not exist.'}), 409
     query = "INSERT INTO airline_staff VALUES(%s, %s, %s, %s, %s, %s)"
-    password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-    cursor.execute(query, (username, password, first_name, last_name, dob, airline))
+    hashed_password = bcrypt.hashpw(new_account["password"].encode('utf8'), bcrypt.gensalt())
+    cursor.execute(query, (new_account["username"], hashed_password, new_account["first_name"], 
+                           new_account["last_name"], new_account["dob"], new_account["airline"]))
     conn.commit()
     cursor.close()
-    session['username'] = username
     return jsonify({'status': 'Successfully registered airline staff'}), 200
 
 
-@app.route("/login-customer", methods=["GET", "POST"])
+@app.route("/login-customer", methods=["POST"])
 def login_customer():
-    email = request.json["email"]
-    password = request.json["password"]
+    user_json = request.json
     cursor = conn.cursor()
     query = "SELECT * FROM customer WHERE email=%s"
-    cursor.execute(query, (email))
+    cursor.execute(query, (user_json["email"]))
     user = cursor.fetchone()
-    if not user or not bcrypt.checkpw(password.encode('utf8'), user['acc_password'].encode("utf8")):
+    if not user or not bcrypt.checkpw(user_json["password"].encode('utf8'), user['acc_password'].encode("utf8")):
         return jsonify({'status': 'Incorrect Email or Password'}), 403
-    session['username'] = email
     return jsonify({'status': 'Successfully logged in.'}), 200
 
-@app.route("/login-airline-staff")
+
+@app.route("/login-airline-staff", methods=['POST'])
 def login_airline_staff():
-    username = request.json["username"]
-    password = request.json["password"]
+    user_json = request.json
+    cursor = conn.cursor()
+    query = "SELECT * FROM airline_staff WHERE username=%s"
+    cursor.execute(query, (user_json["username"]))
+    user = cursor.fetchone()
+    if not user or not bcrypt.checkpw(user_json["password"].encode('utf8'), user['acc_password'].encode('utf8')):
+        return jsonify({'status': 'Incorrect Username or Password'}), 403
     return jsonify({'status': 'Successfully logged in.'}), 200
 
 
-@app.route("/get-session", methods=["GET"])
-def get_session():
-    print(session['username'])
-    if 'username' not in session:
-        return jsonify({'status': 'Session not found.'}), 404
-    return jsonify({'session': session['username']}), 200
-
-
-@app.route("/logout")
-def log_out():
-    session.pop("username")
-    return jsonify({"Status": "Logged out"}), 200
-
-
-app.secret_key = 'sdknljdghdhdsDhd1445GKse6g6hfL7f3f8s11s33788sJS'
 if __name__ == "__main__":
     app.run("127.0.0.1", 5000, debug=True)
